6a7d15207f67949dbfcc64eac5cd6842dff3db9b
xinghuoyoucun <417498119@qq.com>
2019-09-06 12:35:50 +0800

    搜索注册到nacos

---
+++ hello-spring-cloud-elasticsearch-achievement/src/main/java/com/laiyuan/hello/spring/cloud/elasticSearch/achievement/vo/ProjectVO.java
@@ -0,0 +1,10 @@
+package com.laiyuan.hello.spring.cloud.elasticSearch.achievement.vo;
+
+/**
+ * @author by laiyuan
+ * @Date 2019/9/6 11:24
+ * @Description: TODO
+ * @Version 1.0
+ */
+public class ProjectVO {
+}
